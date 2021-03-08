36802b2ed8a98438f19293a68ee7db4ada40aae1
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-03-08 16:41:25 +0000

    finished Item

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -9,24 +9,32 @@ loadDataTable = () => {
 		ajax: {
 			url: '/api/book',
 			type: 'Get',
-			dataType: 'json',
+			'dataType': 'json',
 		},
-		columns: [
-			{ data: 'name', width: '30%' },
-			{ data: 'author', width: '30%' },
-			{ data: 'isbn', width: '30%' },
+		"columns": [
+			{ "data": "name", "width": "30%" },
+			{ "data": "author", "width": "30%" },
+			{ "data": "isbn", "width": "30%" },
 			{
-				data: 'id',
-				render: function (data) {
+				"data": "id",
+				"render": function (data) {
 					return `
 						<div class='text-center'>
 							<a
 								href='/BookList/Edit?id=${data}'
-								class='btn btn-success text-white'></a>
+								class='btn btn-success text-white' style="cursor: pointer; width: 100px;">Edit</a>
+								&nbsp;
+								<a
+								class='btn btn-danger text-white' style="cursor: pointer; width: 100px;">Delete</a>
 						</div>
 					`;
 				},
+				"width": "30%",
 			},
 		],
+		"language": {
+			"emptyTable": "No data Found",
+		},
+		"width": "100%",
 	});
 };
